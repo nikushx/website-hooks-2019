29ec221aff46d161d5c942a00560e810de55fc1c
Nikush Dalia <nikushdalia@gmail.com>
2019-05-02 18:05:12 -0400

    updated README.md

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
+import useGlobalHookCreator from 'use-global-hook';
 import './App.css';
 
+const initialGlobalState = {
+  testCounter: 0
+}
+
+const globalActions = {
+  addToTestCounter: (store, testParam) => {
+    store.setState({
+      testCounter: store.state.testCounter + 1
+    })
+  }
+}
+
+const useGlobal = useGlobalHookCreator(React, initialGlobalState, globalActions);
+
 function App() {
+
+  const [globalStore, globalActions] = useGlobal();
+
+  const addToCounter = () => globalActions.addToTestCounter();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Counter: {globalStore.testCounter}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={addToCounter}>Add to Counter</button>
       </header>
     </div>
   );
